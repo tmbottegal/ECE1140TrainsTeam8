f95b4840ca41542af9878f08f9720abff9926505
tmbottegal <tmbottegal@gmail.com>
2025-11-11 19:37:47 -0500

    created main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+# Set up sys.path for module imports
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'CTC'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'trackControllerHW'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'trackControllerSW'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'trackModel'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'trainModel'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'trainControllerSW'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'universal'))
+
+
+# CTC import
+
+# Wayside Controller import
+
+# Track Model import
+from trackModel.track_model_backend import TrackNetwork
+from trackModel.track_model_frontend import NetworkStatusUI
+# Train Model import
+
+# Train Controller import
+
+# Universal import
+
+# PyQt6 import
+from PyQt6.QtWidgets import QApplication
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    network = TrackNetwork()
+    network.load_track_layout('trackModel/green_line.csv')
+    TrackModelUI = NetworkStatusUI(network)
+    TrackModelUI.show()
+    app.exec()

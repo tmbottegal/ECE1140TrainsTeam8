04dc80327f5122df8fbe6abc39266deb9bbdc57d
Jay <jay7fu@gmail.com>
2025-12-11 16:40:33 -0500

    uncomment display hw wayside

--- main.py
+++ main.py
@@ -68,20 +68,22 @@ if __name__ == "__main__":
         }
     for ctrl in controllers.values(): 
         ctrl.start_live_link(poll_interval=1.0)
+        
+    
     #------------------------------------------------------------------------------------------------
     # track controller hw
-    # hw_controllers = {
-    # "Green Line": HardwareTrackControllerBackend(network1, "Green Line"),
+    hw_controllers = {
+    "Green Line": HardwareTrackControllerBackend(network1, "Green Line"),
     # Add Red Line if needed:
-    # "Red Line": HardwareTrackControllerBackend(network2, "Red Line"),
-# }
+    "Red Line": HardwareTrackControllerBackend(network2, "Red Line"),
+}
 
-    # for ctrl in hw_controllers.values():
-    #     ctrl.start_live_link(1.0)
+    for ctrl in hw_controllers.values():
+        ctrl.start_live_link(1.0)
 
-    # hw_ui = TrackControllerHWUI(hw_controllers)
-    # hw_ui.setWindowTitle("Wayside Controller – Hardware UI")
-    # hw_ui.show()
+    hw_ui = TrackControllerHWUI(hw_controllers)
+    hw_ui.setWindowTitle("Wayside Controller – Hardware UI")
+    hw_ui.show()
     #-----------------------------------------------------------------------------------------------
     TrackModelUI = NetworkStatusUI(network1, network2)
     TrackModelUI.show()
